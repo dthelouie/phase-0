46a552dc73f6794c22dbb982b9fa9ef86d387c07
David Louie <dlouie1492@gmail.com>
2015-11-30 16:00:09 -0500

    Complete challenge 4.3

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,23 @@
+
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ sum = num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ diff = num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ product = num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ quotient = num_1.to_f / num_2.to_f
+end
