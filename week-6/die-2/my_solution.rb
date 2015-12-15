16367a3b0c934f32e7897111949e29595b654062
David Louie <dlouie1492@gmail.com>
2015-12-15 04:55:55 -0500

    Complete challenge 6.2

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -71,3 +71,17 @@ end
 
 
 
+# Reflection
+# 1. What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+# => In the original challenge, the only instance variable that was used was the number of sides,
+# => but in this challenge there needed to be an instance variable for the labels that were
+# => passed through the argument.  Other than that, the implementation was generally the same.
+# 2. What does this exercise teach you about making code that is easily changeable or modifiable? 
+# => Making code that is easily changeable is important because if some sort of new condition
+# => rises, you need to be able to adapt your code to the condition with a simple solution.
+# 3. What new methods did you learn when working on this challenge, if any?
+# => I used the sample method, which returns a random element from within the array that it's applied to.
+# 4. What concepts about classes were you able to solidify in this challenge?
+# => Classes and their methods need to be able to address multiple situations. Originally,
+# => our die was only able to return integers, but now because it can address multiple situations,
+# => it can return other elements.
