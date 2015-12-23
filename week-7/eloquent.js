3310d4cfe7a9ccd8233dc79c985d5443f0a4975d
David Louie <dlouie1492@gmail.com>
2015-12-23 12:28:05 -0500

    Complete group challenge

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -71,7 +71,9 @@ var me = {
 // 6. What causes a variable to return undefined?
 // A variable will return as undefined when you call a variable that is empty or doesn't exist.
 // 7. Write your own variable and do something to it in the eloquent.js file.
-// See line 8
+// var age = 23;
+// age = age + 1
+// console.log(age)
 // 8. What does console.log do and when would you use it? What Ruby method(s) is this similar to?
 // console.log writes the following expressions into the console.  You would use it to visually
 // represent your results rather than returning them.  This is similar to the Ruby method "puts"
@@ -87,7 +89,22 @@ var me = {
 // Javascript's objects are defined with "var", while you can just declare them in Ruby.
 // Javascript uses "else if" instead of Ruby's "elsif"
 // 12. Complete at least one of the exercises (Looping a Triangle, FizzBuzz, of Chess Board) in the eloquent.js file.
-// See line 14
+// var string = "#"
+// for (var counter = 0; counter < 7; counter++){
+//   console.log(string)
+//   string = string + "#"
+// }
+
+// for (var counter = 1; counter <= 100; counter += 1){
+//   if (counter % 3 == 0 && counter % 5 == 0)
+//     console.log("FizzBuzz");
+// 	else if (counter % 3 == 0)
+//       console.log("Fizz");
+// 	else if (counter % 5 == 0)
+//       console.log("Buzz");
+//     else
+//       console.log(counter);
+//       }
 // 13. What are the differences between local and global variables in JavaScript?
 // Local variables can only be used within functions, and global variables can be used anywhere in a program
 // 14. When should you use functions?
@@ -96,11 +113,20 @@ var me = {
 // A function declaration names the function, takes an argument if necessary, and points it to a 
 // block of code that will run if the function is called.
 // 16. Complete the minimum exercise in the eloquent.js file.
-// See line 42
+// function min(val1, val2){
+// if (val1 < val2)
+//   return val1;
+// else
+//   return val2;
+// }
 // 17. What is the difference between using a dot and a bracket to look up a property? Ex. array.max vs array["max"]
 // Dot notation can be used with valid identifiers, while bracket notation can be used with strings.
 // 18. Create an object called me that stores your name, age, three favorite foods, and a quirk in your eloquent.js file.
-// See line 52
+// var me = {
+// 	name: "David Louie"
+// 	favorite_foods: ["Burgers", "Steak", "Pizza"]
+// 	quirk: "Owns a ton of star wars lego sets"
+// }
 // 19. What is a JavaScript object with a name and value property similar to in Ruby?
 // Objects in JavaScript are just like Hashes in Ruby.  They have names that point to other values.
 
